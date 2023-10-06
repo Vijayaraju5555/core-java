de23a1d34da5ca0e5f02dc137e21607bc6eecbfc
acer <acer@TMP249-G2-M>
2023-10-06 16:24:16 +0530

    First Commit

--- Project1/src/com/tns/interfaces/Demo.java
+++ Project1/src/com/tns/interfaces/Demo.java
@@ -2,16 +2,10 @@ package com.tns.interfaces;
 
 public class Demo implements ChildInterface, InterfaceOne {
 	
-
-	@Override
 	public void print() {
 		System.out.println("print method");
 	}
 	
-	
-	
-
-	@Override
 	public void show() {
 		System.out.println("show method");
 	
